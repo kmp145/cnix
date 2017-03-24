b5f705e3721bb68e0230b520a4bd3a00047cc90d
Kmp145 <adamlock998@gmail.com>
2017-03-24 05:43:07 -0400

    Added a basic config loaded. 	-needs to modified to handle comments and errors

--- Assignment/files.c
+++ Assignment/files.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "io.h"
 #include "logger.h"
 
+struct settings_t{
+	char placeholder[20];
+};
+
+struct settings_t loadSettings(){
+	struct settings_t settings;
+	FILE *fp;
+	fp = fopen("settings.conf", "r");
+	if(fp){//if the file exists and is readable
+		fread(&settings, sizeof(settings), 1, fp);
+		fclose(fp);
+	}
+	else{//if the file does not exist return create file with default settings
+		fopen("settings.conf", "w");
+		strcpy(settings.placeholder,"defaultSettings");
+		fwrite(&settings, sizeof(settings), 1, fp);
+		fclose(fp);	
+	}
+	return settings;
+}
+
 int main(){
+	struct settings_t settings;
+	settings = loadSettings();
+	printf("%s\n",settings.placeholder);
 	return 0;
 }
